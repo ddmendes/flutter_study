c23f2dfd4aced8047377e4307673b09d6312c6ab
Davi Mendes <ddioriomendes@gmail.com>
2020-08-04 22:20:43 -0300

    Flutter Course lessons 54 and 55

---
+++ dicee/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/dmendes/.bin/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/dmendes/Projects/studies/flutter_study/dicee"
+export "FLUTTER_TARGET=/Users/dmendes/Projects/studies/flutter_study/dicee/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/dmendes/.bin/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
